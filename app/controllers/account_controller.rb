aedf860d7cb8386f2b533a03a898ad33258de646
Verizent <michael.richard.suryajaya@gmail.com>
2026-03-25 23:15:18 +0800

    feat: add signup logic (before otp implementation)

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,5 +1,58 @@
 class AccountController < ApplicationController
-    # GET /account/signup
-    def signup
+  # GET /account/signup
+  def signup
+    @user = User.new
+  end
+
+  # POST /account/signup
+  def create_user
+    @user = User.new(signup_params)
+    @user.faculty = @user.faculty&.reject(&:blank?)
+    @user.department = @user.department&.reject(&:blank?)
+
+    unless params[:terms_accepted].present?
+      flash.now[:alert] = "You must accept the Terms & Conditions to register."
+      render :signup, status: :unprocessable_entity and return
     end
+
+    if @user.save
+      raw_code = @user.generate_otp!
+      # OtpMailer.send_code(@user, raw_code).deliver_later
+      puts raw_code
+      session[:pending_user_id] = @user.id
+      redirect_to signup_verify_path, notice: "Check your CUHK email for a 6-digit verification code."
+    else
+      render :signup, status: :unprocessable_entity
+    end
+  end
+
+  # GET /account/verify => page to input OTP
+  def verify
+  end
+
+  # POST /account/verify => verify otp
+  def verify_signup_otp
+  end
+
+  # GET /account/login
+  def login
+  end
+
+  # POST /account/login
+  def authenticate
+  end
+
+  # GET /account/2fa
+  def two_factor
+  end
+
+  # POST /account/2fa
+  def verify_2fa
+  end
+
+  private
+
+  def signup_params
+    params.require(:user).permit(:name, :email, :college, :password, :password_confirmation, faculty: [], department: [])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  # Run before saving to database
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true,
+            format: { with: /\A[^@]+@(\w+\.)*cuhk\.edu\.hk\z/i, message: "must be a CUHK email address" }
+  validates :college, presence: true
+  validates :faculty, presence: true
+  validates :department, presence: true
+  validates :password, length: { minimum: 12 }, allow_nil: true
+
+  # Class Constants
+  OTP_EXPIRY_MINUTES = 10
+  MAX_OTP_ATTEMPTS = 3
+
+  # Class Methods
+  def generate_otp!
+    raw_code = SecureRandom.random_number(10**6).to_s.rjust(6, "0")
+    # Update User's OTP info
+    update!(otp_code: raw_code, otp_sent_at: Time.current, otp_attempts: 0)
+    raw_code
+  end
+
+  def otp_valid?(submitted_code)
+    return false if otp_code.blank? || otp_sent_at.blank?
+    return false if otp_sent_at < OTP_EXPIRY_MINUTES.minutes.ago
+    return false if otp_attempts >= MAX_OTP_ATTEMPTS
+    otp_code == submitted_code
+  end
+
+  def verified?
+    verified_at.present?
+  end
+end
