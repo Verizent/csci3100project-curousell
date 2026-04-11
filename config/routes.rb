6de19e566490ad9a2f782e147ef5a1f2a56142f4
kanta14706 <kanta14706@gmail.com>
2026-04-12 07:04:06 +0800

    routes.rb remove conflict 4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: redirect("/home")
 
   get "/home" => "listings#index", as: :home
-<<<<<<< HEAD
+  get "/orders"  => "orders#index",  as: :orders
   resources :listings, only: [ :index, :show, :new, :create ]
   resources :feedback, only: [ :create ]
 
