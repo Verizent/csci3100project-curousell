710ebdb8b02689d416e8bc6ad0833f141b1cbcd3
kanta14706 <kanta14706@gmail.com>
2026-04-13 03:47:30 +0800

    application_controller.rb merge issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,22 +9,13 @@ class ApplicationController < ActionController::Base
   end
   
   def require_login
-<<<<<<< Updated upstream
     return true # for testing purposes, remove this line in production
     unless logged_in?
       redirect_to account_signin_path, alert: "Please log in to continue."
     end
-=======
-    # Do nothing for now - always return a user
-    current_user
->>>>>>> Stashed changes
   end
-<<<<<<< Updated upstream
 
   def authenticate_user!
     require_login
   end
 end
-=======
-end
->>>>>>> Stashed changes
