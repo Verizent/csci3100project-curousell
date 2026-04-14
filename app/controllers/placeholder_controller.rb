a090177710c4b51f336e1b591dc48c766f6c8ef1
kanta14706 <kanta14706@gmail.com>
2026-04-14 22:57:12 +0800

    remove placeholder controller since no longer used

--- app/controllers/placeholder_controller.rb
+++
@@ -1,9 +0,0 @@
-class PlaceholderController < ApplicationController
-  def chats
-    render plain: "Chats — coming soon", status: :ok
-  end
-
-  def orders
-    render plain: "Orders — coming soon", status: :ok
-  end
-end
